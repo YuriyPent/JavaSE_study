c2f70050b581d2c91e1f5208fd3924ef79683885
Yuriy <Seaman_on_watch@mail.ru>
2019-02-03 22:00:47 +0300

    Графический вывод на основе класса Canvas

--- JavaApplication1/src/basejava/Painting.java
+++ JavaApplication1/src/basejava/Painting.java
@@ -1,33 +1,36 @@
 package basejava;
 
 import java.awt.Button;
+import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Frame;
-import java.awt.Graphics;
-import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Painting extends Frame implements ActionListener {
 
     private String s;
+    private Canvas cs = new Canvas();
     Button print = new Button("Print");
     Button clear = new Button("Clear");
     Button exit = new Button("Exit");
 
-    public Painting() throws HeadlessException {
+    Painting() {
 
         setLayout(null);//размещение элементов выполнить вручную
         s = "Hello From frame!";
         add(print);
         add(clear);
         add(exit);
+        add(cs);
         print.addActionListener(this);
         clear.addActionListener(this);;
         exit.addActionListener(this);
         print.setBounds(150, 90, 100, 20);
         clear.setBounds(150, 120, 100, 20);
         exit.setBounds(150, 150, 100, 20);
+        cs.setBounds(150, 180, 120, 20);
+        cs.setBackground(Color.yellow);
     }
 
     @Override
@@ -37,17 +40,19 @@ public class Painting extends Frame implements ActionListener {
             System.exit(0);
         } else {
             if (e.getSource() == print) {
-                Graphics z = getGraphics();
-                z.drawString(s, 150, 200);
+                int x = (int) Math.round((Math.random() * 20 + 10));
+                int y = (int) Math.round((Math.random() * 100 + 110));
+                cs.move(x, y);
+                cs.getGraphics().drawString(s, 10, 10);
             } else {
                 if (e.getSource() == clear) {
-                    Graphics z = getGraphics();
-                    z.clearRect(8, 110, 110, 35);//Очистка и заливка цветом
+                    cs.getGraphics().clearRect(0, 0, 170, 135);
                 }
             }
         }
     }
 
+    @SuppressWarnings("deprecation")
     public static void main(String[] args) {
         Painting pt = new Painting();
         pt.resize(400, 400);
