a55c53031d6488a9a3bc277cf7e10bc361ed2391
Yuriy <Seaman_on_watch@mail.ru>
2019-02-10 17:48:35 +0300

    Work with popup window

--- JavaApplication1/src/basejava/applet/Forma.java
+++ JavaApplication1/src/basejava/applet/Forma.java
@@ -1,69 +1,68 @@
 package basejava.applet;
 
+import java.awt.AWTEvent;
 import java.awt.Color;
-import java.awt.Event;
-import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
 
 @SuppressWarnings("serial")
-public class Forma extends JFrame implements ActionListener, MouseListener{
+public class Forma extends JFrame implements ActionListener, MouseListener {
 
-    JMenuBar mb = new JMenuBar();
-    JMenu m1 = new JMenu("Operations");
-    JMenu m2 = new JMenu("TheEnd");
+    JButton exit = new JButton("Exit");
+    JPopupMenu menu = new JPopupMenu("Pop");//Всплывающее меню
+    JMenuItem it1 = new JMenuItem("Show");
+    JMenuItem it2 = new JMenuItem("Quit");
 
-    public Forma() throws HeadlessException {
+    Forma() {
 
-        m1.add(new JMenuItem("Show"));
-        m2.add(new JMenuItem("Quit"));
-        mb.add(m1);
-        mb.add(m2);
-        setJMenuBar(mb);
-        setBackground(new Color(200, 20, 100));
-    }
+        setLayout(null);
+        add(exit);
+        addMouseListener(this);
 
-    public boolean action(Event evt, Object ob) {
-        String lbl = (String) ob;
-        if (lbl.equals("Quit")) {
-            System.exit(0);
-            return true;
-        } else {
-            if (lbl.equals("Show")) {
-                Framemenu fr = new Framemenu();
-                fr.setSize(300, 300);
-                fr.setVisible(true);
-                return true;
-            }
-        }
-        return false;
-    }
-    
-    public static void main(String[] args) {
-        Forma f =new Forma();
-        f.setSize(400, 400);
-        f.setVisible(true);
+        add(menu);
+        menu.add(it1);
+        menu.add(it2);
+        it1.addActionListener(this);
+        it2.addActionListener(this);
+
+        enableEvents(AWTEvent.MOUSE_EVENT_MASK);//Активизация событий от мыши
+        exit.addActionListener(this);
+        exit.setBounds(10, 20, 100, 20);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == exit) {
+            System.exit(0);
+        } else {
+            if (e.getSource() == it2) {
+                setBackground(Color.yellow);
+                Framemenu fr = new Framemenu();
+                fr.setSize(300, 300);
+                fr.setVisible(true);
+            }
+        }
     }
 
     @Override
-    public void mouseClicked(MouseEvent e) {}
+    public void mouseClicked(MouseEvent e) {
+    }
 
     @Override
     public void mousePressed(MouseEvent e) {
     }
 
     @Override
-    public void mouseReleased(MouseEvent e) {
+    public void mouseReleased(MouseEvent ev) {
+        if (ev.isPopupTrigger()) {
+            menu.show(ev.getComponent(), ev.getX(), ev.getY());
+        }
     }
 
     @Override
@@ -72,5 +71,12 @@ public class Forma extends JFrame implements ActionListener, MouseListener{
 
     @Override
     public void mouseExited(MouseEvent e) {
+    }
+
+    public static void main(String[] args) {
+        Forma f = new Forma();
+        f.setSize(400, 400);
+        f.setBackground(new Color(100, 60, 120));
+        f.setVisible(true);
     }
 }
--- JavaApplication1/src/basejava/applet/Framemenu.java
+++ JavaApplication1/src/basejava/applet/Framemenu.java
@@ -1,21 +1,18 @@
 package basejava.applet;
 
-import java.awt.Color;
 import java.awt.Event;
 import java.awt.Graphics;
-import java.awt.HeadlessException;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-class Framemenu extends JFrame {
+class Framemenu extends JFrame { //Класс вспомогательной формы
 
     JButton ex = new JButton("Return");
 
-    Framemenu() throws HeadlessException {
+    Framemenu() {
         add(ex);
         setLayout(null);
         ex.setBounds(20, 40, 100, 20);
-        setBackground(new Color(100, 60, 120));
     }
 
     public void paint(Graphics g) {
@@ -25,7 +22,7 @@ class Framemenu extends JFrame {
     public boolean action(Event evt, Object ob) {
         String lb = (String) ob;
         if (lb.equals("Return")) {
-            dispose();
+            dispose();//Закрыть и удалить вспомогательную форму
         }
         return true;
     }
